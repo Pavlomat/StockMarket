ec644245c0ab015e2207ba4cf89ecd883508eb79
Pavlomat <75501963+Pavlomat@users.noreply.github.com>
2021-03-18 16:38:50 +0300

    Add files via upload

---
+++ PavlovTest/DetailShareCell.swift
@@ -0,0 +1,16 @@
+//
+//  DetailShareCell.swift
+//  PavlovTest
+//
+//  Created by Pavlov Matthew on 24.02.2021.
+//
+
+import UIKit
+
+//создвю макет строки для Detail VC
+class DetailShareCell: UITableViewCell {
+
+    @IBOutlet var parameterName: UILabel!
+    @IBOutlet var parameterInfo: UILabel!
+    @IBOutlet var parameterStats: UILabel!
+}
